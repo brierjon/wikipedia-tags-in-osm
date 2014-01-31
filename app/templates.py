cec5e9cf8c81342027b25ee74b8017e4a464ed06
CristianCantoro <kikkocristian@gmail.com>
2014-01-31 20:36:50 +0100

    Handle coordinates insertion if a template is already there

--- app/templates.py
+++ app/templates.py
@@ -22,11 +22,15 @@
 #  If not, see <http://www.gnu.org/licenses/>.
 
 import re
+import wikipedia_template_parser as wtp
 from coords import coords_template
+from utils import coords_deg2dms_cp, format_dms
 
 HEADING = re.compile(r'==[^=]+==')
 
-COORD = re.compile('{{coord(.*)}}', re.IGNORECASE)
+COORD = re.compile(r'\{\{coord[^{}]*?', re.IGNORECASE)
+
+CLOSE_TEMPLATE = re.compile(r'[^{}]*?(\}\})?')
 
 
 import ast
@@ -36,38 +40,47 @@ with open('../data/wikipedia/coords/templates_including_coords.txt',
 
 
 def find_coords_templates(old_text):
-    template = None
+    template_found = {}
 
     coord = COORD.search(old_text)
     if coord:
-        template = 'coord'
+        template_found['name'] = 'coord'
+        template_found['has_coords'] = True
     else:
         for t in TEMPLATES:
-            search_regex = '{{%s(.*)}}' % t['name']
-            s = re.search(search_regex, old_text, re.IGNORECASE)
+            search_regex = r'{{%s(.*)' % t['name']
+            match = re.search(search_regex, old_text, re.IGNORECASE)
 
-            if 'rifugio' in t['name'].lower():
-                import pdb
-                pdb.set_trace()
+            if match:
+                templates = wtp.data_from_templates(page='',
+                                                    lang='',
+                                                    wikitext=old_text
+                                                    )
+                tmpl = [template['data']
+                        for template in templates
+                        if template['name'].lower() == t['name'].lower()
+                        ]
+                data = tmpl and tmpl[0] or None
 
-            if s:
-                import pdb
-                pdb.set_trace()
+                template_found['name'] = t['name']
+                template_found['has_coords'] = False
 
-    return template
+                for p in t['parameters']:
+                    if data and p in data:
+                        if data[p]:
+                            template_found['has_coords'] = True
+
+    return template_found
 
 
-def get_new_text(lat, lon, dim, old_text='', template=None):
+def get_new_text_no_template(lat, lon, dim, old_text=''):
     new_text = old_text
     section = 0
 
+    incipit = None
     first_heading = HEADING.search(old_text)
     if first_heading:
         incipit = old_text[:first_heading.start()]
-
-    if template:
-        # do something
-        pass
 
     else:
         tmpl = coords_template(lat=lat,
@@ -85,14 +98,194 @@ def get_new_text(lat, lon, dim, old_text='', template=None):
     return new_text, old_text, section
 
 
+def _get_element(name):
+    ELEMENTS = {"_d": 'dec',
+                "gradi": 'd',
+                "minuti": 'm',
+                "secondi": 's',
+                "ns": 'cp',
+                "ew": 'cp',
+                "verso": 'cp'
+                }
+
+    el = 'dec'
+    for k in ELEMENTS.keys():
+        if k in name:
+            el = ELEMENTS[k]
+            break
+
+    return el
+
+
+def get_parameter(name, lat, lon):
+
+    dms = coords_deg2dms_cp(lat, lon)
+    dms = format_dms(dms)
+
+    name = name.lower()
+    if 'lat' in name:
+        el = _get_element(name)
+
+        if el == 'dec':
+            return str(lat)
+        else:
+            return str(dms['lat'][el])
+
+    elif ('lon' in name) or ('lng' in name):
+        el = _get_element(name)
+
+        if el == 'dec':
+            return str(lon)
+        else:
+            return str(dms['lon'][el])
+    else:
+        return "Caso inatteso, non salvare questa pagina e segnalalo "\
+               "segnalalo agli sviluppatori. Grazie."
+
+
+def get_new_text_with_template(lat, lon, dim, template, old_text=''):
+    section = 0
+
+    if template['name'] == 'coord':
+        coord = COORD.search(old_text)
+
+        start_template = coord.start()
+
+        end_parameter = coord.end()
+
+        match_close = CLOSE_TEMPLATE.search(old_text[end_parameter:],
+                                            re.MULTILINE
+                                            )
+        end_template = end_parameter + match_close.end()
+
+        new_template = coords_template(lat, lon, dim)
+
+        new_text = old_text[:start_template] + new_template + \
+            old_text[end_template:]
+
+    else:
+        new_template_text = ''
+
+        search_regex = r'{{%s(.*)' % template['name']
+        match_template = re.search(search_regex, old_text, re.IGNORECASE)
+
+        # template starts after parentesis and name
+        start_template = match_template.start()
+        start_template_data = match_template.start() + 2 + \
+            len(template['name'])
+
+        coord_params = [t['parameters']
+                        for t in TEMPLATES
+                        if t['name'].lower() == template['name'].lower()
+                        ][0]
+
+        templates = wtp.data_from_templates(page='',
+                                            lang='',
+                                            wikitext=old_text
+                                            )
+        tmpl = [t['data']
+                for t in templates
+                if t['name'].lower() == template['name'].lower()
+                ]
+
+        data = tmpl and tmpl[0] or None
+
+        max_parameter = 0
+        for k in data.keys():
+            search_parameter = r'%s\s*=\s*' % k
+            match_parameter = re.search(search_parameter,
+                                        old_text[start_template_data:],
+                                        re.IGNORECASE
+                                        )
+            end_parameter = match_parameter.end()
+            if end_parameter > max_parameter:
+                max_parameter = end_parameter
+
+        end_last_parameter = start_template_data + max_parameter
+
+        match_close = CLOSE_TEMPLATE.search(old_text[end_last_parameter:],
+                                            re.IGNORECASE
+                                            )
+
+        if match_close:
+            end_template = end_last_parameter + match_close.end()
+            new_template_text = old_text[start_template:end_template]
+
+            if template['has_coords']:
+                for p in coord_params:
+                    search_parameter = r'%s\s*=\s*' % p
+                    match_parameter = re.search(search_parameter,
+                                                new_template_text,
+                                                re.IGNORECASE
+                                                )
+                    parameter_end = match_parameter.end()
+
+                    search_parameter = r'(.*)'
+                    match_parameter = re.search(
+                        search_parameter,
+                        new_template_text[parameter_end:],
+                        re.IGNORECASE)
+
+                    line_end = match_parameter.end()
+
+                    value_start = parameter_end
+                    value_end = value_start + line_end
+
+                    new_parameter = get_parameter(p, lat, lon)
+
+                    new_template_text = new_template_text[:value_start] + \
+                        new_parameter + \
+                        new_template_text[value_end:]
+
+                    new_text = old_text[:start_template] + \
+                        new_template_text + old_text[end_template:]
+            else:
+                new_template_text = new_template_text[:-2]
+                new_line = '|{par_name} = {par_value}\n'
+                for p in coord_params:
+                    par_value = get_parameter(p, lat, lon)
+                    new_parameter = new_line.format(par_name=p,
+                                                    par_value=par_value)
+                    new_template_text += new_parameter
+
+                new_template_text += '}}'
+
+                new_text = old_text[:start_template] + \
+                    new_template_text + old_text[end_template:]
+
+    return new_text, old_text, section
+
+
+def __test(in_, out_):
+
+    with open(in_, 'r') as infile:
+        wikitext = infile.read().decode('utf-8')
+
+    template = find_coords_templates(wikitext)
+
+    new_text, old_text, section = get_new_text_with_template(
+        lat=45.990001,
+        lon=9.800001,
+        dim=100,
+        template=template,
+        old_text=wikitext)
+
+    d = difflib.HtmlDiff()
+    diff = d.make_file(old_text.split('\n'), new_text.split('\n'))
+
+    with codecs.open(out_, 'w+', 'utf-8') as outfile:
+        outfile.writelines(diff)
+
+
 if __name__ == '__main__':
-    with open('test/torre_pendente_di_pisa.txt', 'r') as infile:
-        pisa = infile.read()
+    import difflib
+    import codecs
 
-    with open('test/rifugio_laghi_gemelli.txt', 'r') as infile:
-        rifugio = infile.read()
+    __test(in_='test/torre_pendente_di_pisa.txt',
+           out_='torre_pendente_di_pisa.html')
 
-    find_coords_templates(rifugio)
+    __test(in_='test/rifugio_laghi_gemelli.txt',
+           out_='rifugio_laghi_gemelli.html')
 
-    import pdb
-    pdb.set_trace()
+    __test(in_='test/diga_di_pieve_di_cadore.txt',
+           out_='diga_di_pieve_di_cadore.html')
